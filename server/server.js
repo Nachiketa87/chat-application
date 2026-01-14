e6a9e8aedfe60628a3d396cca359848573a31db1
Nachiketa87 <keshu61bn@gmail.com>
2026-01-14 19:37:07 +0530

    backend-models,endpoints

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import http from 'http';
+import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+
+
+const app = express();
+const server = http.createServer(app);
+
+//middlewaree
+app.use(express.json({limit:"4mb"}));
+app.use(cors());
+
+
+//route
+app.use("/api/status",(req,res)=>res.send("server is live"));
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter);
+
+//mongodb
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>console.log("server is running on PORT: "+PORT));
