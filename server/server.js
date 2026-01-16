359e0301129ca983b15fa24d2fd8b02d71a3c2bb
Nachiketa87 <keshu61bn@gmail.com>
2026-01-17 01:00:37 +0530

    vercel mods

--- server/server.js
+++ server/server.js
@@ -46,5 +46,9 @@ app.use("/api/messages",messageRouter);
 //mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=>console.log("server is running on PORT: "+PORT));
+if(process.env.NODE_ENV != "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=>console.log("server is running on PORT: "+PORT));
+}
+//export to vercel
+export default server;
